a7de3471e3c570a4e7d7232fc5150f2e21e23774
SandraMCodes <sandram97@hotmail.com>
2024-11-30 12:09:10 +0800

    log in zod

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,8 +1,16 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import z from "zod";
 
 import postLogin from "../api/post-login.js";
 import { useAuth } from "../hooks/use-auth.js";
+
+const loginSchema = z.object({
+   username: z.string().min(1, { message: "Username must not be empty" }),
+   password: z
+     .string()
+     .min(8, { message: "Password must be at least 8 characters long" }),
+ });
 
 function LoginForm() {
     const navigate = useNavigate();
@@ -21,21 +29,22 @@ function LoginForm() {
           };
         
 
-             const handleSubmit = (event) => {
-                   event.preventDefault();
-                   if (credentials.username && credentials.password) {
-                       postLogin(
-                           credentials.username,
-                          credentials.password
-                       ).then((response) => {
-                        window.localStorage.setItem("token", response.token);
-                         setAuth({
-                              token: response.token,
-                               });
-                        navigate("/");
-                      });
-                   }
-               };
+          const handleSubmit = (event) => {
+            event.preventDefault();
+            const result = loginSchema.safeParse(credentials);
+            if (!result.success) {
+              const error = result.error.errors?.[0];
+              if (error) {
+                alert(error.message);
+              }
+              return;
+            } else {
+              postLogin(result.data.username, result.data.password).then((response) => {
+                window.localStorage.setItem("token", response.token);
+                navigate("/");
+              });
+            }
+          };
     return (
       <form>
         <div>
